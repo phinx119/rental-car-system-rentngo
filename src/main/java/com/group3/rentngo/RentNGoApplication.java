1171b3ae26be074805564c156d57378f52f05d14
PhiNX <xuanphi2003@gmail.com>
2024-03-28 15:56:37 +0700

    first commit

---
+++ src/main/java/com/group3/rentngo/RentNGoApplication.java
@@ -0,0 +1,13 @@
+package com.group3.rentngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentNGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentNGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group3/rentngo/RentNGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.rentngo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentNGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
