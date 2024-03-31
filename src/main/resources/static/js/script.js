761bfd61627c2d4686ca21009125cec44745b600
Phongldm <phongldmhe171478@fpt.edu.vn>
2024-03-31 12:55:01 +0700

    update

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,9 +1,27 @@
- document.addEventListener('DOMContentLoaded', function () {
+//Popup logout window
+document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('logoutBtn').addEventListener('click', function (event) {
         event.preventDefault();
 
         if (confirm('Are you sure you want to log out?')) {
-            window.location.href = 'login.html';
+            window.location.href = 'home-page.html';
         }
     });
 });
+
+//Change between thumbnail view and list view
+ const thumbnailViewBtn = document.getElementById('thumbnailViewBtn');
+ const listViewBtn = document.getElementById('listViewBtn');
+ const thumbnailView = document.getElementById('thumbnailView');
+ const listView = document.getElementById('listView');
+
+ // Add event listeners to buttons
+ thumbnailViewBtn.addEventListener('click', () => {
+     thumbnailView.style.display = 'block';
+     listView.style.display = 'none';
+ });
+
+ listViewBtn.addEventListener('click', () => {
+     thumbnailView.style.display = 'none';
+     listView.style.display = 'block';
+ });
