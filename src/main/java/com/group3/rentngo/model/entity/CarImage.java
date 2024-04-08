8eb01cc50fb98345a320a757925648cca0832975
PhiNX <xuanphi2003@gmail.com>
2024-04-08 23:03:08 +0700

    update relationship between car and car_image

--- src/main/java/com/group3/rentngo/model/entity/CarImage.java
+++ src/main/java/com/group3/rentngo/model/entity/CarImage.java
@@ -35,8 +35,4 @@ public class CarImage {
     @Lob
     @Column(nullable = false)
     private Blob rightImage;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "car_id")
-    private Car car;
 }
